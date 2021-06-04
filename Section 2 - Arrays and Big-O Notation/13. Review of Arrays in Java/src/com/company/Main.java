31dace59eaf011e4b9d62bb8eae56799c9068930
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-06-05 00:24:53 +0530

    Review of Arrays

---
+++ Section 2 - Arrays and Big-O Notation/13. Review of Arrays in Java/src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] intArray = new int[7];
+
+        intArray[0] = -20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 44;
+        intArray[4] = 60;
+        intArray[5] = -21;
+        intArray[6] = 89;
+
+        for (int i = 0; i<intArray.length; i++){
+
+        }
+
+
+
+    }
+}
