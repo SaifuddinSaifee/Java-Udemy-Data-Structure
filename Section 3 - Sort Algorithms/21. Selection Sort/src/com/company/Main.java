93c911242949fee68fb55bb45c31aba577e69297
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-06-10 14:22:00 +0530

    Completed Selection Sort

--- Section 3 - Sort Algorithms/21. Selection Sort/src/com/company/Main.java
+++ Section 3 - Sort Algorithms/21. Selection Sort/src/com/company/Main.java
@@ -3,6 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastUnsortedArrayIndex = intArray.length - 1; lastUnsortedArrayIndex > 0; lastUnsortedArrayIndex--) {
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedArrayIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+                swap(intArray, largest, lastUnsortedArrayIndex);
+            }
+        }
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
     }
 }
