e63575c89ab8a98c81158c09e5dab140910577ac
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-06-10 14:05:23 +0530

    Completing Bubble Sort

--- Section 3 - Sort Algorithms/17. Bubble Sort/src/com/company/Main.java
+++ Section 3 - Sort Algorithms/17. Bubble Sort/src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {20, 35, -15, 7, 55, 1, -22};
+
+        for (int lastIntSortedIndex = arr.length - 1; lastIntSortedIndex > 0; lastIntSortedIndex--) {
+            for (int i = 0; i < lastIntSortedIndex; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
     }
 }
