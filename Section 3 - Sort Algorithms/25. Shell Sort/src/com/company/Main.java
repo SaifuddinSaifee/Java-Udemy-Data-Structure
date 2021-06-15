0e936081fed9487e617deba5fac6f47fa5b828ad
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-06-15 21:43:39 +0530

    Adding another method or another type of approach to sorting an array)* *Not Shell Sort

--- Section 3 - Sort Algorithms/25. Shell Sort/src/com/company/Main.java
+++ Section 3 - Sort Algorithms/25. Shell Sort/src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        int[] arr = {14, 18, 19, 37, 23, 40, 29, 30, 11};
+//        System.out.println(Arrays.toString(anotherApproach(arr)));
+        for (int gap = arr.length/2; gap>0; gap/=2){
 
+        }
+
+    }
+
+
+
+
+
+
+
+    public static int[] anotherApproach(int[] arr) {
+        int N = arr.length;
+        int gap = (int) Math.floor(N / 2);
+        for (int i = 0; i >= 0; i++) {
+            int j = i + gap;
+            while (j < N) {
+                if(arr[i] >= arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+            gap = (int) Math.floor(gap / 2);
+        }
+        return arr;
     }
 }
