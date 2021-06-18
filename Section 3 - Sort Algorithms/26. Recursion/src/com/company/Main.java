6411509480ce3e9d436a1990d30491c7ecc10135
Saifuddin Saifee <saifuddin.saifee@live.co.uk>
2021-06-18 21:39:35 +0530

    Factorial Code Completed

--- Section 3 - Sort Algorithms/26. Recursion/src/com/company/Main.java
+++ Section 3 - Sort Algorithms/26. Recursion/src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println(fact(3));
+
+        System.out.println(recursiveFact(3));
+    }
+
+    public static int recursiveFact(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * recursiveFact(num - 1);
+    }
+
+    public static int fact(int num) {
+        if (num == 0) {
+            return 1;
+        }
+
+        int factorial = 1;
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        return factorial;
     }
 }
